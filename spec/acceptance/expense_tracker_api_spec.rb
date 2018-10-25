408e8973adf97c1912ee2dafca00aa4dcc0faa58
Hatem Khattab <hattemkhattab@gmail.com>
2018-10-25 11:35:34 +0200

    Add first test

---
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,16 @@
+require 'rack/test'
+require 'json'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+    it 'record submitted expenses' do
+      coffe = {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2017-06-10'
+      }
+      post '/expenses', JSON.generate(coffe)
+    end
+  end
+end
