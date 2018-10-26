388cb0c66e84137d8cf4f66d2f8c10e4d81e9448
Hatem Khattab <hattemkhattab@gmail.com>
2018-10-26 12:10:52 +0200

    Add api test

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,11 @@
+module ExpenseTracker
+  RSpec.describe API do
+    describe 'post /expenses' do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id'
+        it 'responds with a 200 (OK)'
+      end
+      # .. next context will go here
+    end
+  end
+end
